2f954cd8dfcd8baaf90d4a85370700f1d44bb2f6
Michael L Miller <mike@Michael-Millers-MacBook-Gold.local>
2018-06-20 17:24:39 -0400

    First commit

---
+++ Bootstrap/js/hoverplay.js
@@ -0,0 +1,15 @@
+// something along that
+var vid = document.getElementsByTagName("video");
+[].forEach.call(vid, function (item) {
+	item.addEventListener('mouseover', hoverVideo, false);
+	item.addEventListener('mouseout', hideVideo, false);
+});
+
+function hoverVideo(e)
+{	
+	this.play();
+}
+function hideVideo(e)
+{
+	this.pause();
+}
